a34b3e194c68207e13d7983d97b4c9e56638eec7
himaloyash <himaloyashis98@gmail.com>
2023-05-26 17:02:49 +0600

    OurMenu added

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../shared/Footer/Footer';
 import NavBar from '../shared/Navbar/NavBar';
+import { Helmet } from 'react-helmet-async';
 
 const Main = () => {
     return (
