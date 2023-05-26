efbaa898cc135e315bfba9fc22e3c41971401fd4
himaloyash <himaloyashis98@gmail.com>
2023-05-26 17:41:31 +0600

    parallax added

--- src/shared/Cover/Cover.jsx
+++ src/shared/Cover/Cover.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
-const Cover = ({image, title}) => {
+import { Parallax, Background } from 'react-parallax';
+
+const Cover = ({ image, title }) => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("${image}")` }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">{title}</h1>
-                    <p className="mb-5">Would you like to our dish.</p>
-                   
+        <Parallax
+            blur={{ min: -50, max: 50 }}
+            bgImage={image}
+            bgImageAlt="the dog"
+            strength={-200}
+        >
+            <div className="hero h-[500px]">
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+                        <p className="mb-5">Would you like to our dish.</p>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax>
+
     );
 };
 
