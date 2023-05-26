410f177770b0eb31e93e0cf3a3052758bbe9d4ac
himaloyash <himaloyashis98@gmail.com>
2023-05-26 13:16:16 +0600

    data faced

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -3,12 +3,14 @@ import Footer from '../../shared/Footer/Footer';
 import NavBar from '../../shared/Navbar/NavBar';
 import Slider from './Slider/Slider';
 import Banner from './Banner/Banner';
+import Server from './server/Server';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <Banner></Banner>
+            <Server></Server>
         </div>
     );
 };
---
+++ src/Page/Home/server/Server.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './server.css'
+
+const Server = () => {
+    return (
+        <div className='serve py-10'>
+            <div className='w-2/3 text-center mx-auto py-3 rounded bg-white'>
+                <p className='text-3xl'>Bistro Boss</p>
+                <h1>chef service</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem molestias enim eum aut iusto at, quia totam? Labore nostrum, blanditiis autem libero illo sapiente, distinctio cumque, voluptatibus voluptatum perferendis eaque. Maiores, repellendus minima optio est necessitatibus id ratione consequatur incidunt maxime, ab perferendis sed totam doloremque. Esse nam saepe corporis.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Server;
