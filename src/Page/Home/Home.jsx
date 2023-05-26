3ec5b645bf18e8d2a3721f74d5fe8a3d58350912
himaloyash <himaloyashis98@gmail.com>
2023-05-26 12:36:09 +0600

    json upload done

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from '../../shared/Footer/Footer';
 import NavBar from '../../shared/Navbar/NavBar';
 import Slider from './Slider/Slider';
+import Banner from './Banner/Banner';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <Banner></Banner>
         </div>
     );
 };
